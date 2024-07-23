7d21474abce5d01d6aee2b934ea3d117f14eb71a
chimichubanga <chimichubanga@vk.com>
2024-07-23 23:35:16 +0300

    the logger is disabled

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,18 @@
 import logging
 
 def setup_logging():
-    # Настройка основного логирования
+    # Настройка основного логирования с уровнем INFO
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
     # Установка уровней логирования для модулей discord.py
-    logging.getLogger('discord.client').setLevel(logging.WARNING)
-    logging.getLogger('discord.gateway').setLevel(logging.WARNING)
-    logging.getLogger('discord.player').setLevel(logging.WARNING)
-    logging.getLogger('discord.voice_client').setLevel(logging.WARNING)
+    #logging.getLogger('discord.client').setLevel(logging.WARNING)
+    #logging.getLogger('discord.gateway').setLevel(logging.WARNING)
+    #logging.getLogger('discord.player').setLevel(logging.WARNING)
+    #logging.getLogger('discord.voice_client').setLevel(logging.WARNING)
     
     # Логирование для состояния голосовых каналов
-    logging.getLogger('discord.voice_state').setLevel(logging.ERROR)
+    #logging.getLogger('discord.voice_state').setLevel(logging.ERROR)
+
+# Вызов функции для настройки логирования
+setup_logging()
